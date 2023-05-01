d0f8ea4920b0b3b9edb919447bbdbc1f1e7a9ef9
MhdMansoor <muhammed.m@codelynks.com>
2023-05-01 13:38:03 +0530

    - Created Class for news api request and response and setup retrofit for Api call.

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "957b264a70dc4699bd5a669e46de3894"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
